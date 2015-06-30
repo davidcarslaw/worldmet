5ac0fe01446edf3fc09e32c546c60404f4b4c14a
David Carslaw <david.carslaw@york.ac.uk>
2015-06-30 20:22:49 +0100

    ensure full year

--- R/exportADMS.R
+++ R/exportADMS.R
@@ -15,7 +15,13 @@
 exportADMS <- function(dat, out = "./ADMS_met.MET") {
   
   ## make sure the data do not have gaps
-  dat <- openair:::date.pad2(dat, interval = "hour")
+  all.dates <- data.frame(date = seq(ISOdatetime(year = as.numeric(format(dat$date[1], "%Y")), 
+                                                 month = 1, day = 1, hour = 0, min = 0, sec = 0, tz = "GMT"),
+                                     ISOdatetime(year = as.numeric(format(dat$date[1], "%Y")), 
+                                                 month = 12, day = 31, hour = 23, min = 0, 
+                                                 sec = 0, tz = "GMT"), by = "hour"))
+  
+  dat <- merge(dat, all.dates, all = TRUE)
   
   ## exports met data to ADMS format file
     year <- as.numeric(format(dat$date, "%Y"))
