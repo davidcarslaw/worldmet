18b30c34c386d4f4ecaf1f8b7f76e0707f6c466a
David Carslaw <david.carslaw@york.ac.uk>
2015-06-30 08:35:40 +0100

    add exportADMS function

---
+++ R/exportADMS.R
@@ -0,0 +1,31 @@
+#' Export a meteorological data frame in ADMS format
+#'
+#' @param dat A data frame imported by \link{\code{importNOAA}}.
+#' @param out A file name for the ADMS file. The file is written in the working directory by default.
+#'
+#' @return Writes a text file to a location of the user's choosing.
+#' @export
+#'
+exportADMS <- function(dat, out = "./ADMS_met.MET") {
+  
+    ## exports met data to ADMS format file
+    year <- as.numeric(format(dat$date, "%Y"))
+    day <- as.numeric(format(dat$date, "%j"))
+    hour <- as.numeric(format(dat$date, "%H"))
+    station <- "0000"
+    
+    adms <- data.frame(station, year, day, hour, round(dat$air_temp, 1), 
+                       round(dat$ws, 1), round(dat$wd, 1), round(dat$RH, 1),
+                       as.integer(dat$cl))
+    
+    ## replace NA with -999
+    adms[] <- suppressWarnings(lapply(adms, function(x) replace(x, is.na(x), -999)))
+    
+    write.table(adms, file = out, col.names = FALSE, row.names = FALSE, sep = ",", quote = FALSE)
+    
+    fConn <- file(out, 'r+') 
+    Lines <- readLines(fConn) 
+    writeLines(c("VARIABLES:\n9\nSTATION DCNN\nYEAR\nTDAY\nTHOUR\nT0C\nU\nPHI\nRH\nCL\nDATA:", Lines), con = fConn) 
+    close(fConn) 
+  
+}
